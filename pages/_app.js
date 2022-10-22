86536455e77f7856ff9ccb58f3b0ef8af852ad90
Ariful54 <72780478+Ariful54@users.noreply.github.com>
2022-10-22 22:10:06 +0600

    Arif

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Navbar></Navbar>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
